ea15e17bcf1adeb33ccca1c17e7110857d1bc1d0
petehunt <floydophone@gmail.com>
2014-07-15 10:40:12 -0700

    Source maps?

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ var reactTools = require('react-tools');
 var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
-  this.cacheable && this.cacheable()
+  this.cacheable && this.cacheable();
 
+  var filename = loaderUtils.getRemainingRequest(this);
   var query = loaderUtils.parseQuery(this.query);
+
   if (query.insertPragma) {
     source = '/** @jsx ' + query.insertPragma + ' */' + source;
   }
 
   return reactTools.transform(source, {
-    harmony: query.harmony
+    harmony: query.harmony,
+    sourceMap: query.sourceMap,
+    sourceFilename: filename
   });
 };
