0f564a99c71b960ca8c241ba8659a05180555cce
Jake Riesterer <jake@impossibleventures.com>
2014-08-25 21:46:24 -0500

    Implement sourcemaps

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ var loaderUtils = require('loader-utils');
 module.exports = function(source) {
   this.cacheable && this.cacheable();
 
-  var filename = loaderUtils.getRemainingRequest(this);
   var query = loaderUtils.parseQuery(this.query);
-
   if (query.insertPragma) {
-    source = '/** @jsx ' + query.insertPragma + ' */' + source;
+    source = '/** @jsx ' + query.insertPragma + ' */\n' + source;
   }
 
-  return reactTools.transform(source, {
+  var result = reactTools.transformWithDetails(source, {
     harmony: query.harmony,
-    sourceMap: query.sourceMap,
-    sourceFilename: filename
+    sourceMap: query.sourceMap
   });
+
+  if (result.sourceMap) {
+    result.sourceMap.sources = [this.resourcePath];
+    result.sourceMap.sourcesContent = [source];
+  }
+
+  var code = result.code;
+  var map = result.sourceMap ? JSON.stringify(result.sourceMap) : void 0;
+
+  this.callback(null, code, map);
 };
