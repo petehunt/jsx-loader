2fe3fba8d80068fe566c6321ee3b896758ddb04a
Jake Riesterer <jake@impossibleventures.com>
2014-08-27 09:46:48 -0500

    Remove sourceMap flag and instead always emit them

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(source) {
 
   var result = reactTools.transformWithDetails(source, {
     harmony: query.harmony,
-    sourceMap: query.sourceMap
+    sourceMap: true
   });
 
   if (result.sourceMap) {
