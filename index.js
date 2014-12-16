427aa8e17c5e54b101bd205a453058f7de6f9f41
Mark Finger <markfinger@gmail.com>
2014-12-16 11:45:47 +1100

    Improving the clarity of errors that occur during the JSX transformation.
    Errors will now report the source file that triggered the error.

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ module.exports = function(source) {
     source = '/** @jsx ' + query.insertPragma + ' */' + source;
   }
 
-  var transform = reactTools.transformWithDetails(source, {
-    harmony: query.harmony,
-    stripTypes: query.stripTypes,
-    es5: query.es5,
-    sourceMap: this.sourceMap
-  });
+  try {
+    var transform = reactTools.transformWithDetails(source, {
+      harmony: query.harmony,
+      stripTypes: query.stripTypes,
+      es5: query.es5,
+      sourceMap: this.sourceMap
+    });
+  } catch(err) {
+    err.message = 'JSX loader failed on ' + sourceFilename + ': ' + err.message;
+    throw err;
+  }
   if (transform.sourceMap) {
     transform.sourceMap.sources = [sourceFilename];
     transform.sourceMap.file = current;
